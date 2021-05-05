e14cf6ad277ca456784997979425a44567e3e8ce
clementineviolette <clementine.violette@gmail.com>
2021-05-05 11:43:43 +0200

    avec connexion

--- page-accueil.php
+++ page-accueil.php
@@ -27,14 +27,37 @@
         <h4 class="card-title mb-2">Voici les règles du jeu </h4>
         
         <p class="card-text" style="text-align: justify;"> <br>
-          Vous avez jusqu’au début du TD, soit environ trois semaines, pour saisir vos Données dans l’onglet « Saisir mes données », cela permettra d’avoir des données plus fiables et de gagner du temps sur la réalisation du TD. <br><br>
-          Vous pourrez ensuite commencer le TD avec les données préalablement fournies. Attention : lorsque vous cliquez sur "Commencer le TD", le chronomètre est lancé, et vous avez 2 heures pour effectuer le TD. Vous aurez 3 essais par question. <br><br>
-          Vous pourrez ensuite télécharger un compte rendu personnalisé où figureront votre note et les détails apports/dépenses de votre semaine type.
+          Vous avez jusqu’au début du TD, soit environ trois semaines, pour saisir vos Données dans l’onglet <b>"Saisir mes données"</b>. Cette démarche est nécessaire pour le déroulé du TD. Ainsi, vous vous devez de remplir sérieusement vos informations personnelles de repas et niveau d'activité sportive sur une semaine. Si vous décidez de ne pas remplir l'intégralité de vos données, <b>vous perdrez 3 point sur le résultat final</b> (votre TD sera noté sur 17). <br><br>
+          Sur le créneau de TD qui vous est dédié, vous aurez accès à <b>"Commencer le TD"</b>. Une fois confirmé, vous n'aurez plus la possibilité de revenir en arrière avant la fin du TD. Vous disposez de 3 essais par question, au delà desquels vous obtiendrez 0 à la question. Sur les deux premières tentaives fausses, vous perdez 0.15 point et un message d'aide apparaît pour vous guider vers le résultat attendu. <br><br>
+          Une fois le TD fini, vous aurez accès à votre compte rendu personnalisé et illustré avec les points de cours essentiels et votre note. Ce dernier sera accessible dans l'onglet <b>"Accéder à mon compte rendu et mes notes"</b>.
         </p>
       </div>
     
       <div class="d-grid gap-2 d-md-block text-center boutons">
         <a class="btn btn-lg text-center btn-custom" href="./menu-saisies-donnees.php">Saisir mes données</a>
+
+        <!-- Modal, permet d'afficher une fenetre js pour RGPD-->
+        <!-- Actuellement soucis car s'affiche à chaque fois que l'on clique sur le bouton "saisir mes données" -->
+        <div class="modal fade" id="accord_rgpd" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title w-100 text-center" id="staticBackdropLabel">Attention</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                Acceptez vous de saisir vos habitudes alimentaires et physiques ? <br> Vous retrouverez ces informations sur votre compte-rendu personnel. Ces dernières ne seront stockées que jusqu'à la fin du TD, vous aurez ensuite la possibilité de les supprimer. 
+                
+                <br><br> Si vous refusez de saisir vos habitudes alimentaire et physique, vous bénéficierez d'un jeu de données type et acceptez de perdre 3 points sur la note finale. 
+              </div>
+              <div class="modal-footer justify-content-center">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Refuser</button>
+                <button type="button" class="btn btn-primary"><a href="./partie1-1.php">Accepter</a></button>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <a class="btn btn-lg text-center btn-custom" data-bs-toggle="modal" data-bs-target="#commencer_td"> Commencer le TD </a>
         
         <!-- Modal, permet d'afficher une fenetre js pour confirmer le choix-->
@@ -46,17 +69,17 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body">
-                Voulez-vous vraiment commencer le TD ? <br> Le chronomètre sera lancé dès que vous cliquerez sur "Oui"
+                Voulez-vous vraiment commencer le TD ? <br> Vous ne pourrez plus revenir en arrière avant la fin du TD dès que vous cliquerez sur "Oui"
               </div>
               <div class="modal-footer justify-content-center">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Non</button>
-                <button type="button" class="btn btn-primary"><a href="./partie1-1.php">Oui</a></button> <!--Bouton Oui, il faut rajouter le code pour démarer le chrono et il faut changer et mettre ./partie1-1.php -->
+                <button type="button" class="btn btn-primary"><a href="./partie1-1.php">Oui</a></button>
               </div>
             </div>
           </div>
         </div>
         
-        <a class="btn btn-lg text-center btn-custom" href="./Compte_rendu_et_notes.html">Accéder à mon compte rendu et mes notes</a>
+        <a class="btn btn-lg text-center btn-custom" href="./Compte_rendu_et_notes.php">Accéder à mon compte rendu et mes notes</a>
       </div>
     </div>
 
